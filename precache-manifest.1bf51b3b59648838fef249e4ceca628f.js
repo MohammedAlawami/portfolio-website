492625370c805a3b27a70a8e62cdbe1b4f6e2aa5
Mohammed Alawami <Mrawami@Mohammeds-MacBook-Pro-2013.local>
2019-07-21 02:59:23 -0700

    Updates

---
+++ precache-manifest.1bf51b3b59648838fef249e4ceca628f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "830cc3a25828cf3ee483a5e358d06b92",
+    "url": "/static/media/resume.830cc3a2.pdf"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5837c4f48c160ab7c46f",
+    "url": "/static/js/main.6b50769c.chunk.js"
+  },
+  {
+    "revision": "7905500bf9c9e01fc639",
+    "url": "/static/js/2.1daae6a7.chunk.js"
+  },
+  {
+    "revision": "33db54496dcc93b7e09c8a368da5ec77",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1cf6131f6aaa0c492a965d1c1c0ade9c.js"
+  "/precache-manifest.1bf51b3b59648838fef249e4ceca628f.js"
 );
 
 workbox.clientsClaim();
